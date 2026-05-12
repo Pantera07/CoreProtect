a5561bf00eda3419856159a0015cd5452e65c8fe
Pantera (Mad_Daniel) <89838384+Pantera07@users.noreply.github.com>
2026-05-12 18:53:12 +0900

    Update ListenerHandler.java

--- src/main/java/net/coreprotect/listener/ListenerHandler.java
+++ src/main/java/net/coreprotect/listener/ListenerHandler.java
@@ -181,19 +181,23 @@ public final class ListenerHandler {
         pluginManager.registerEvents(new StructureGrowListener(), plugin);
 
         // Plugin channel events
-        pluginManager.registerEvents(new PluginChannelListener(), plugin);
+        //pluginManager.registerEvents(new PluginChannelListener(), plugin); // Zerus
     }
 
     public static void registerNetworking() {
+        /* Zerus
         CoreProtect.getInstance().getServer().getMessenger().registerIncomingPluginChannel(CoreProtect.getInstance(), PluginChannelHandshakeListener.pluginChannel, new PluginChannelHandshakeListener());
         CoreProtect.getInstance().getServer().getMessenger().registerOutgoingPluginChannel(CoreProtect.getInstance(), PluginChannelHandshakeListener.pluginChannel);
         CoreProtect.getInstance().getServer().getMessenger().registerOutgoingPluginChannel(CoreProtect.getInstance(), PluginChannelListener.pluginChannel);
+        */
     }
 
     public static void unregisterNetworking() {
+        /* Zerus
         CoreProtect.getInstance().getServer().getMessenger().unregisterIncomingPluginChannel(CoreProtect.getInstance(), PluginChannelHandshakeListener.pluginChannel);
         CoreProtect.getInstance().getServer().getMessenger().unregisterOutgoingPluginChannel(CoreProtect.getInstance(), PluginChannelHandshakeListener.pluginChannel);
         CoreProtect.getInstance().getServer().getMessenger().unregisterOutgoingPluginChannel(CoreProtect.getInstance(), PluginChannelListener.pluginChannel);
+        */
     }
 
 }
