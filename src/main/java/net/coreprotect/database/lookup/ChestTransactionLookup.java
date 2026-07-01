3a273772a7b88e09688e91b10baf694d7ddf74f0
Pantera <89838384+Pantera07@users.noreply.github.com>
2026-07-01 16:27:41 +0900

    Implement player location validation in performLookup
    Added player location checks for command sender in performLookup method.

--- src/main/java/net/coreprotect/database/lookup/ChestTransactionLookup.java
+++ src/main/java/net/coreprotect/database/lookup/ChestTransactionLookup.java
@@ -22,6 +22,7 @@ import net.coreprotect.utility.MaterialUtils;
 import net.coreprotect.utility.StringUtils;
 import net.coreprotect.utility.WorldUtils;
 import net.coreprotect.utility.ErrorReporter;
+import org.bukkit.entity.Player;
 
 public class ChestTransactionLookup {
 
@@ -32,6 +33,22 @@ public class ChestTransactionLookup {
             if (l == null) {
                 return result;
             }
+
+            // Zerus start
+            if (commandSender instanceof Player) {
+                Player player = (Player) commandSender;
+
+                Location playerLocation = player.getLocation();
+
+                if (!playerLocation.getWorld().equals(l.getWorld())) {
+                    if (!player.hasPermission("coreprotect.lookup.near")) return result;
+                }
+
+                if (playerLocation.distance(l) > 7) {
+                    if (!player.hasPermission("coreprotect.lookup.near")) return result;
+                }
+            }
+            // Zerus end
 
             if (command == null) {
                 if (commandSender.hasPermission("coreprotect.co")) {
