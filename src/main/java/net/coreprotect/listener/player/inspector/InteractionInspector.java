47451100770b62dee13767a8f311b251c85af551
Pantera <89838384+Pantera07@users.noreply.github.com>
2026-09-14 04:17:24 +0900

    Implement location checks in InteractionInspector
    Added location checks to ensure player and block are in the same world and within 8 blocks distance.

--- src/main/java/net/coreprotect/listener/player/inspector/InteractionInspector.java
+++ src/main/java/net/coreprotect/listener/player/inspector/InteractionInspector.java
@@ -3,6 +3,7 @@ package net.coreprotect.listener.player.inspector;
 import java.sql.Connection;
 import java.sql.Statement;
 
+import org.bukkit.Location; // Zerus
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -17,6 +18,17 @@ public class InteractionInspector extends BaseInspector {
             @Override
             public void run() {
                 try {
+                    // Zerus start
+                    Location playerLocation = player.getLocation();
+                    Location blockLocation = finalInteractBlock.getLocation();
+                    if (!playerLocation.getWorld().equals(blockLocation.getWorld())) {
+                        return;
+                    }
+                    if (playerLocation.distance(blockLocation) > 8) {
+                        return;
+                    }
+                    // Zerus end
+
                     try (Connection connection = getDatabaseConnection(player)) {
                         Statement statement = connection.createStatement();
                         String blockData = InteractionLookup.performLookup(null, statement, finalInteractBlock, player, 0, 1, 7);
